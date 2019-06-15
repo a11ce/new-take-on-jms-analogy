e44e8eb0dbbb84490858cbea22fe8b41197b49e6
a11ce <a11ce@protonmail.com>
2019-06-15 14:48:53 -0400

    Polish classic word ladder and add readme

---
+++ classicWordLadder/README.md
@@ -0,0 +1,7 @@
+# Classic Word Ladder
+
+## How to play
+
+1. Run `python3 server.py` on either players computer, or a separate one, which will print `ready at <server ip>:<port>`. The port is hardcoded to 1224.
+2. On both players computers, run `nc <server ip> 1224`
+3. The game will start when both players connect
--- classicWordLadder/server.py
+++ classicWordLadder/server.py
@@ -1,9 +1,11 @@
 import sys
 sys.path.append("..")
-import wordGenesUtil as util
 import socket
 import threading
 import time
+import os
+
+import wordGenesUtil as util
 
 # State vals
 clients = []
@@ -24,33 +26,36 @@ class ThreadedServer(object):
         while True:
             client, address = self.sock.accept()
             client.settimeout(600)
+            
             threading.Thread(target = self.listenToClient,args = (client,address)).start()
-
+            
     def listenToClient(self, client, address):
-        size = 1024
-        print("Client connected from " + str(address))
-        clientNum = len(clients)
-        clients.append(client)
+        try:
+            size = 1024
+            print("Client connected from " + str(address))
+            clientNum = len(clients)
+            clients.append(client)
 
-        if not (len(clients) == 2):
-            client.send(b"Waiting for other player to connect...\n")            
-            while not len(clients) == 2:
-                continue
-            client.send(b"Other player connected!\n\n")
-            beginGame()
-        print("Waiting for inp")
+            if not (len(clients) == 2):
+                client.send(b"Waiting for other player to connect...\n")            
+                while not len(clients) == 2:
+                    continue
+                client.send(b"Other player connected!\n\n")
+                beginGame()
 
-        winner = False
+            winner = False
                 
-        while True:
+            while True:
         
-            answer = client.recv(1024)
-            ansPath = answer.decode("utf-8").rstrip().split(" ")
-            if(util.validatePath(ansPath) and ansPath[0] == words[0] and ansPath[-1] == words[-1]):
-                endGame(clientNum)
-            else:
-                client.send(b"No\n> ")
-        
+                answer = client.recv(1024)
+                ansPath = answer.decode("utf-8").rstrip().split(" ")
+                if (util.validatePath(ansPath) and ansPath[0] == words[0] and ansPath[-1] == words[-1]):
+                    endGame(clientNum)
+                else:
+                    client.send(b"No\n> ")
+        except:
+            print("SOMETHING HAS GONE WRONG. PLEASE TRY AGAIN.")
+            os._exit(1)
 def beginGame():
 
     sendToAll("Complete the word ladder as fast as possible\n")
@@ -63,12 +68,15 @@ def beginGame():
         time.sleep(1)
     sendToAll(str(words[0] + " TO " + words[1] + "\n> "))
     gameStarted = True
+    print("game started")
 
 def endGame(winnerNum):
+    print("game ended")
     clients[winnerNum    ].send(b"You won")
     clients[1 - winnerNum].send(b"\nYou lost")
     for client in clients:
         client.close()
+    os._exit(0)
 
 def sendToAll(obj):
     for client in clients:
@@ -79,7 +87,10 @@ if __name__ == "__main__":
     startWord = util.randomWord()
     endWord = util.randomPathWithLength(startWord, 5)[-1]
     words = (startWord, endWord)
-    print(words)
-    print("ready")
+    #print(words)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    print("ready at " + s.getsockname()[0] +":1224")
+    s.close()
     ThreadedServer('', 1224).listen()
     
